e151321a79258580782c7f02a603faf11da72897
okoth-099 <fokothdev1@gmail.com>
2024-09-12 04:47:08 -0700

    First python commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+
+number1 = 10
+number2 = 5
+
+sum = number2 + number1
+difference = number2 - number1
+product = number1 * number2
+
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subraction of", number2, "and", number1, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
