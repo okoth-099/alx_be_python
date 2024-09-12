ae47a23a8acd18902811614dae0b084e22745c8d
okoth-099 <fokothdev1@gmail.com>
2024-09-12 05:57:25 -0700

    Edited simple calc

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
-P = 1000
-R = 0.05
-T = 3
+principal = 1000
+rate = 0.05
+time = 3
 
-I = P * R * T 
+interest = principal * rate * time 
 
-print("The simple interest is:", I)
+print("The simple interest is:", interest)
