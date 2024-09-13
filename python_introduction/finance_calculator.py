3af6c8b7325fd7ebd42dd03670fe88c9e97891dc
okoth-099 <fokothdev1@gmail.com>
2024-09-13 06:42:33 -0700

    Personal calc bug fiexed

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,5 +3,5 @@ monthly_expenditure = int(input("Enter your total monthly expenses:"))
 monthly_savings = monthly_income - monthly_expenditure
 project_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
-print("Your monthly savings are $", monthly_savings, ".")
-print("Projected savings after one year, with interest, is: ","$", int(project_savings), ".")
+print(f"Your monthly savings are ${monthly_savings}.")
+print(f"Projected savings after one year, with interest, is: ${projected_saving}.")
