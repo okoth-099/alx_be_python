d33fd46b3d172d4124347276b810bc85c0359b2e
okoth-099 <fokothdev1@gmail.com>
2024-09-21 12:28:14 -0700

    multipication table app

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -5,7 +5,7 @@ weather = input("What's the weather like today? (sunny/rainy/cold):").lower()
 if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
-    print("Don't forget your umbrellla and raincoat")
+    print("Don't forget your umbrella and raincoat")
 elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf")
 else:
