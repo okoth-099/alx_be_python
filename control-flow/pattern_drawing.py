83cd42f25c4ca021f46c4b59e704c353c66aabc8
okoth-099 <fokothdev1@gmail.com>
2024-09-22 06:42:23 -0700

    patterm drawing app

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,12 @@
+# patern drawing app
+
+number = int(input("Enter the size of pattern: "))
+
+c = 1
+
+while c <= number:
+    for i in range(number):
+        print("*", end="")
+    print()
+    c+=1
+        
