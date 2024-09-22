aa5dfd87369e26ebf0497affaeb93149d2acff28
okoth-099 <fokothdev1@gmail.com>
2024-09-22 13:54:52 -0700

    Edited the drawing app

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,10 +1,10 @@
 #pattern app
-length = int(input("Enter the size of pattern: "))
+pattern = int(input("Enter the size of pattern: "))
 
 c = 1
 
-while c <= length:
-    for i in range(length):
+while c <= pattern:
+    for i in range(pattern):
        print("*", end="")
     print()
     c+=1
