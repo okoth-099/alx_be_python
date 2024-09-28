680a716a03c0d4972b61463c956031b2e222776a
Okoth <fokothdev1.gmail.com>
2024-09-29 01:08:04 +0300

    bug module fixed

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,14 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            try:
-                return num1 / num2
-            except ZeroDivisionErrror:
-                return "Error: Cannot divide by zero!"
-        case _:
-            "Operation does not exist"
+    if perform_operation == "add":
+        return num1 + num2
+    if perform_operation == "subtract":
+        return num1 - num2
+    if perform_operation == "multiply":
+        return num1 * num2
+    if perform_operation == "divide":
+        try:
+            return num1 / num2
+        except ZeroDivisionErrror:
+            return "Error: Cannot divide by zero!"
+    else:
+        "Operation does not exist"
