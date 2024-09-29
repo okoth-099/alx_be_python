ec845d4367971f25be13cd314ecb977ab0027d01
Okoth <fokothdev1.gmail.com>
2024-09-29 12:41:47 +0300

    library use

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,24 @@
+#Part 1: Display the Current Date and Time
+
+
+import datetime
+
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    print(current_date)
+
+#Part 2: Calculate a Future Date
+
+# Function to calculate future date
+def calculate_future_date(days):
+    current_date = datetime.date.today()  # Get current date
+    future_date = current_date + datetime.timedelta(days=days)  # Add days to the current date
+    return future_date
+
+# Prompt user for input
+days = int(input("Enter the number of days: "))
+
+# Calculate and print the future date
+future_date = calculate_future_date(days)
+print(f"The future date is: {future_date}")
+
