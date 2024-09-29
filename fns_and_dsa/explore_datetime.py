50e0df8193d92c99a65f585836caa248340fa485
Okoth <fokothdev1.gmail.com>
2024-09-29 19:53:57 +0300

    date explorer bug fix

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -7,7 +7,7 @@ def display_current_datetime():
 
 #Part 2: Calculate a Future Date
 
-number_of_days = int(input("Enter the number of days: "))
+number_of_days = int(input("Enter the number of days to add to the current date: "))
 
 
 
