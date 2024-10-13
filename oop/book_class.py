885b51b5d65313d5de31cfcabb64acd8d996fcca
Okoth <fokothdev1.gmail.com>
2024-10-13 22:46:29 +0300

    fix on magic

--- oop/book_class.py
+++ oop/book_class.py
@@ -6,15 +6,14 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
-        print(f"Book '{self.title}' has been created.")
 
     def __del__(self):
         """Destructor that prints a message when a Book instance is deleted."""
-        print(f"Deleting '{self.title}'")
+        print(f"Deleting {self.title}")
 
     def __str__(self):
         """String representation of the Book for easy reading."""
-        return f"'{self.title}' by {self.author}, published in {self.year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
         """Official string representation of the Book that can recreate the object."""
@@ -26,11 +25,12 @@ if __name__ == "__main__":
     my_book = Book("1984", "George Orwell", 1949)
 
     # String representation (__str__)
-    print(str(my_book))  # Output: '1984' by George Orwell, published in 1949
+    print(str(my_book))  # Output: 1984 by George Orwell, published in 1949
 
     # Official representation (__repr__)
     print(repr(my_book))  # Output: Book('1984', 'George Orwell', 1949)
 
     # Deleting the book object to invoke __del__
     del my_book
+
 
